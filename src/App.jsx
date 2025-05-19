bc7d8218520148bdeee838170edc9f54d4075533
Fadi-Taleb <faditaleb67@gmail.com>
2025-05-19 17:16:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 
 import React from "react"; 
 
-// import Titre from "./components/Titre.jsx"
-// import Titre1 from "./components/Titre1.jsx"
+ import Titre from "./components/Titre.jsx"
+ import Titre1 from "./components/Titre1.jsx"
   import Compteur from "./components/Compteur";
 import Compteur2 from "./components/Compteur2";
 import Paragraphe from "./components/Paragraphe";
 import Utilisateur from "./components/Utilisateur";
 import Paragraphe1 from "./components/paragraphe1";
- //import Texte from "./components/Texte.jsx";
- //import Paragraph from "./components/Paragraph.jsx";
- //import Formulaire from "./components/Formulaire.jsx";
+ import Texte from "./components/Texte.jsx";
+ import Paragraph from "./components/Paragraph.jsx";
+ import Formulaire from "./components/Formulaire.jsx";
 
  function App()
 {
   return(
   <div>
- {/* <Compteur/> */}
- {/* <Compteur2/> */}
- {/* <Paragraphe/> */}
- {/* <Utilisateur/> */}
+ <Compteur/>
+ <Compteur2/>
+ <Paragraphe/>
+ <Utilisateur/>
  <Paragraphe1/>
 
   </div>
